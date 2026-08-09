94d9482732babf2625d61314019b62c5a9b0d9ed
plb2224 <pritbhasra@gmail.com>
2026-08-09 15:05:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,4 @@
 self.addEventListener('install', (e) => {
-  console.log('App install ho rahi hai');
   self.skipWaiting();
 });
-
-self.addEventListener('fetch', (e) => {
-  // Yeh bas app ko PWA pass (installable) hone ke liye zaroori hota hai
-});
+self.addEventListener('fetch', (e) => {});
